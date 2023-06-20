fc01bb8d4636802bbf9376da54c704800b620424
Song Chanwoo <me@zero0ne.dev>
2023-06-20 17:19:31 +0900

    feat: initial commit

---
+++ main.py
@@ -0,0 +1,24 @@
+from fastapi import FastAPI
+
+import configparser
+
+app = FastAPI()
+
+config = configparser.ConfigParser()
+config.read(filenames="config.ini", encoding="utf-8")
+
+
+@app.get("/")
+async def root():
+    return {"message": "Hello, World!"}
+
+
+if __name__ == "__main__":
+    import uvicorn
+
+    uvicorn.run(
+        host=config["SERVER SETTING"]["HOST"],
+        port=int(config["SERVER SETTING"]["PORT"]),
+        app="main:app",
+        reload=(True if config["SERVER SETTING"]["PORT"] == "True" else False),
+    )
