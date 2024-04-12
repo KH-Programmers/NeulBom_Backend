9ed358657760799f952bbc959e8d8531b70559c0
G1ltchy <raykim1012@gmail.com>
2024-04-12 13:37:33 +0900

    fix: barcodeGenerator transparent background

--- utilities/barcodeGenerator.py
+++ utilities/barcodeGenerator.py
@@ -11,8 +11,9 @@ def GenerateBarcode(stringInput):
     Demonstration :
     입력으로 문자열 stringInput을 받아 Code39 포맷으로 SVG 이미지 반환
     """
-    
-    barcode.base.Barcode.default_writer_options['write_text'] = False
 
+    barcode.base.Barcode.default_writer_options['write_text'] = False
+    barcode.base.Barcode.default_writer_options['background'] = "transparent"
+    
     anImage = Code39(stringInput, add_checksum=False)
     return anImage
