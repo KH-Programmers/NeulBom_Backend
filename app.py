ce7cf29f5b6090d9a4a593f68a8a0452a1f7f054
ZeroOneDeveloper <me@zero0ne.dev>
2024-04-21 14:18:26 +0900

    chore: allowing origins

--- app.py
+++ app.py
@@ -20,6 +20,7 @@ app.add_middleware(
         "http://localhost",
         "http://localhost:3000",
         "http://127.0.0.1:3000",
+        "https://neulbom.wiki"
     ],
     allow_credentials=True,
     allow_methods=["*"],
